b6a53358f44a9d8fbb3374ae84eae24523248b8e
naokiwakata <rockwakata15@gmail.com>
2022-07-26 18:11:54 +0900

    ビデオをロード

---
+++ instance_segmentation.py
@@ -0,0 +1,50 @@
+from dataclasses import field
+import cv2
+from fileEnum import File
+from processImage import processImage
+from predictor import Predictor
+import numpy as np
+from util import shaveOff
+import glob
+
+def instanceSegmentation():
+    imagePath = "images\9_23 (3).JPG"
+    img = cv2.imread(imagePath)  # <class 'numpy.ndarray'>
+
+    predictor = Predictor()
+
+    outputs = predictor.predict(img=img)
+
+    jpgs = glob.glob('testDataLeaf\\*.jpg')
+    for imagePath in jpgs:
+        img = cv2.imread(imagePath)  # <class 'numpy.ndarray'>
+        outputs = predictor.predict(img=img)
+        fields = outputs['instances'].get_fields()
+        pred_boxes = fields['pred_boxes']
+        print(len(pred_boxes))
+        predictor.showPredictImage(img=img, outputs=outputs)
+
+
+
+    shaveOff(outputs=outputs,img=img) #葉っぱのみを切り抜く
+
+    fields = outputs['instances'].get_fields()
+    pred_boxes = fields['pred_boxes']
+    scores = fields['scores'].to('cpu').detach().numpy()
+    pred_classes = fields['pred_classes'].to('cpu').detach().numpy()
+    pred_masks = fields['pred_masks'].to('cpu').detach().numpy()
+
+    image_size = outputs['instances'].image_size
+    height = image_size[0]
+    width = image_size[1]
+
+    predictor.showPredictImage(img=img, outputs=outputs)
+
+    file = File.Image
+    #file = File.Video
+
+    if file == File.Image:
+        #processImage(outputs=outputs, img=img)
+        print(file)
+    elif file == File.Video:
+        print(file)
--- predictor.py
+++ predictor.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2 import model_zoo
@@ -7,6 +8,7 @@ from detectron2.utils.logger import setup_logger
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer
+
 
 class Predictor:
     _metadata = None
@@ -25,27 +27,39 @@ class Predictor:
         cfg.merge_from_file(model_zoo.get_config_file(
             "COCO-InstanceSegmentation\\mask_rcnn_R_50_FPN_3x.yaml"))
         cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # 1クラスのみ
-
         cfg.MODEL.WEIGHTS = os.path.join(
-            cfg.OUTPUT_DIR, "C:\\Users\\wakanao\\detectron2-windows\\model_final.pth")  # 絶対パスでなければならないっぽい
+            cfg.OUTPUT_DIR, "C:\\Users\\wakanao\\projects\\detectron2-windows\\model_final2.pth")  # 絶対パスでなければならないっぽい
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6
         cfg.MODEL.DEVICE = "cpu"
 
         # 予測器を作成
         self._predictor = DefaultPredictor(cfg)
-    
-    def predict(self,img):
+
+    def predict(self, img):
         self._outputs = self._predictor(img)
         return self._outputs
-    
+
     def metadata(self):
         return self._metadata
 
-    def showPredictImage(self,img,outputs):
+    def showPredictImage(self, img, outputs):
         v = Visualizer(img[:, :, ::-1],
-                   metadata=self._metadata,
-                   scale=1.0
-                   )
+                       metadata=self._metadata,
+                       scale=1.0
+                       )
         v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        cv2.imshow('image', v.get_image()[:, :, ::-1])
+        img = v.get_image()[:, :, ::-1]
+        height = int(img.shape[0]/6)
+        width = int(img.shape[1]/6)
+        img = cv2.resize(img, (width, height))
+        cv2.imshow('image', img)
         cv2.waitKey(0)
+
+    def getPredictImage(self, img, outputs):
+        v = Visualizer(img[:, :, ::-1],
+                       metadata=self._metadata,
+                       scale=1.0
+                       )
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        img = v.get_image()[:, :, ::-1]
+        return img
